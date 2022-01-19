dbc735a265c25a352c0a041e59a6e8e00871e23a
Max <mdiazn@uni.pe>
2022-01-18 21:32:56 -0500

    clase 24

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,10 +1,26 @@
 /* eslint-disable react/function-component-definition */
 import React from "react";
+import '../styles/components/Checkout.css';
 
 // eslint-disable-next-line arrow-body-style
 const Checkout = ()=>{
     return (
-        <h1>Checkout</h1>
+        <div className="Checkout">
+            <div className="Checkout-content">
+                <h3>Lista de Pedidos:</h3>
+                <div className="Checkout-item">
+                <div className="Checkout-element">
+                    <h4>ITEM name</h4>
+                    <span>$10</span>
+                </div>
+                <button type="button">Eliminar</button>
+                </div>
+            </div>
+            <div className="Checkout-sidebar">
+                <h3>Precio Total: $10</h3>
+                <button type="button">Continuar pedido</button>
+        </div>
+        </div>
     )
 }
 export default Checkout;
