4ab87888817561e1631ee6abf0a8b8814ea1f86a
Max <mdiazn@uni.pe>
2022-01-19 17:03:35 -0500

    clase 26

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,10 +1,17 @@
 /* eslint-disable react/function-component-definition */
 import React from "react";
+import '../styles/components/Payment.css';
 
 // eslint-disable-next-line arrow-body-style
 const Payment = ()=>{
     return (
-        <h1>Payment</h1>
+    <div className="Payment">
+        <div className="Payment-content">
+        <h3>Resumen del pedido:</h3>
+        <div className="Payment-button">Boton de pago con Paypal</div>
+        </div>
+        <div></div>
+    </div>
     )
 }
 export default Payment;
--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,10 +1,16 @@
 /* eslint-disable react/function-component-definition */
 import React from "react";
-
+import '../styles/components/Success.css';
 // eslint-disable-next-line arrow-body-style
 const Success = ()=>{
     return (
-        <h1>Success</h1>
+    <div className="Succes">
+        <div className="Success-content">
+            <h2>Max, Gracias por tu compra</h2>
+            <span>Tu pedido llegara en 3 dias a tu dirección:</span>
+            <div className="Success-map">Google Maps</div>
+        </div>
+    </div>
     )
 }
 export default Success;
